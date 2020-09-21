4c03543cebb88bb4607703ce71679d94a4a94ac9
MiiSato <64867299+MiiSato@users.noreply.github.com>
2020-09-21 20:16:05 -0300

    EXERCICIO 1 - FERIADO 07/09/20

---
+++ POO/POO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com a largura do retangulo:");
+            double largura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Entre com a altura do retangulo:");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Retangulo retangulo = new Retangulo(largura, altura);
+
+            Console.WriteLine("Area:" + retangulo.GetArea());
+            Console.WriteLine("Perimetro:" + retangulo.GetPerimetro());
+            Console.WriteLine("Diagonal:" + retangulo.GetDiagonal());
+
+            Console.ReadKey();
+        }
+    }
+}
