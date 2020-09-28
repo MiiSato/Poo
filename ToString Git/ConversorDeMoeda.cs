4e3a140a3dc0f01b4a9268cb69a5c806dd073fa3
MiiSato <64867299+MiiSato@users.noreply.github.com>
2020-09-28 07:49:45 -0300

    ToString 07/09

---
+++ ToString Git/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToString
+{
+    class ConversorDeMoeda
+    {
+        public static double GetPrecoFinal(double precoDolar, double qtdDolar)
+        {
+            return (precoDolar * qtdDolar) + ((qtdDolar * 0.06) * precoDolar);
+        }
+    }
+}
---
+++ ToString Git/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotacao do dolar? ");
+            double cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Quantos dolares voce ira comprar? ");
+            double quantidadeDolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Valor a ser pago em reais: R$ " + ConversorDeMoeda.GetPrecoFinal(cotacaoDolar, quantidadeDolar));
+
+            Console.ReadKey();
+        }
+    }
+}
