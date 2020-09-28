6bb17e0dd63e6a48cd71b7cdeeb19174bee7dccd
MiiSato <64867299+MiiSato@users.noreply.github.com>
2020-09-28 08:19:13 -0300

    Exercicio 3

---
+++ POO/Ex3/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double GetNotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+    }
+}
---
+++ POO/Ex3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Aluno:");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as notas do aluno:");
+
+            do
+            {
+                Console.WriteLine("Digite a Nota do primeiro trimeste:");
+                aluno.Nota1 = Convert.ToDouble(Console.ReadLine());
+            } while (aluno.Nota1 < 0 || aluno.Nota1 > 30);
+
+            do
+            {
+                Console.WriteLine("Digite a Nota do segundo trimeste:");
+                aluno.Nota2 = Convert.ToDouble(Console.ReadLine());
+            } while (aluno.Nota2 < 0 || aluno.Nota2 > 35);
+
+            do
+            {
+                Console.WriteLine("Digite a Nota do terceiro trimeste:");
+                aluno.Nota3 = Convert.ToDouble(Console.ReadLine());
+            } while (aluno.Nota3 < 0 || aluno.Nota3 > 35);
+
+            double notaFinal = aluno.GetNotaFinal();
+
+            Console.WriteLine("Nota final:" + notaFinal);
+
+            if(notaFinal >= 60)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO"); 
+                Console.WriteLine("Faltaram: " + (60 - notaFinal) + " pontos.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
