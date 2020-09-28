0e3c619c2f4773e4424efd8a750e044c67c16455
MiiSato <64867299+MiiSato@users.noreply.github.com>
2020-09-28 08:18:02 -0300

    Exercicio 2

---
+++ POO/Ex 2/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double GetSalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100);
+        }
+
+
+    }
+}
---
+++ POO/Ex 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome:");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salario Bruto:");
+            funcionario.SalarioBruto = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Imposto:");
+            funcionario.Imposto = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Funcionario:" + funcionario.Nome + ", " + funcionario.GetSalarioLiquido());
+
+            Console.Write("Digite a porcentagem para aumentar o salario:");
+            funcionario.AumentarSalario(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Dados atualizados:" + funcionario.Nome + ", " + funcionario.GetSalarioLiquido());
+
+            Console.ReadKey();
+        }
+    }
+}
